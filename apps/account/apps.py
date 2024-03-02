66ab9f31c466d24fe115ed17a0cf1feb445f0c23
vadim_p <vadimp_91@mail.ru>
2024-03-02 16:40:33 +0300

    Ads app CRUD view, templates are ready

--- apps/account/apps.py
+++ apps/account/apps.py
@@ -3,6 +3,10 @@
 from django.apps import AppConfig
 
 
+
 class AccountConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.account"
+
+    def ready(self):
+        from apps.account.signals.permissions_signal import update_users_permissions
--- apps/ads/views.py
+++ apps/ads/views.py
@@ -1,4 +1,6 @@
 """Модуль контроллер для приложения ads"""
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views import View
 from django.views.generic import (
@@ -14,46 +16,85 @@ from apps.ads.models import Advertisement
 
 
 class AdvertisementView(View):
-    model = Advertisement
-    context_object_name = "ad"
-    fields = (
-        "name",
-        "channel",
-        "cost",
-        "is_active",
-        # "product",
-    )
-
-
-class AdvertisementCreateView(CreateView):
     queryset = Advertisement.objects.select_related("product")
+    context_object_name = "advertisement"
+
+    def check_permission(self, codename):
+        if self.request.user.is_superuser:
+            return True
+        return any(
+            permission.codename == codename
+            for permission in self.request.user.permissions
+        )
+
+
+class AdvertisementCreateView(AdvertisementView, CreateView):
     form_class = AdvertisementForm
     template_name = "ads/advertisement_create.html"
-    success_url = reverse_lazy("advertisement:advertisement_read")
 
     def get_success_url(self):
         return reverse(
-            "ads:ads_read",
+            "advertisement:advertisement_read",
             kwargs={"pk": self.object.pk},
         )
 
 
-class AdvertisementReadView(DetailView, AdvertisementView):
-    pass
+class AdvertisementReadView(AdvertisementView, DetailView):
+    template_name_suffix = "_read"
+
+    def test_func(self):
+        return self.check_permission("add_advertisement")
 
 
-class AdvertisementUpdateView(UpdateView, AdvertisementView):
-    pass
+class AdvertisementUpdateView(AdvertisementView, UpdateView):
+    success_url = reverse_lazy("advertisement:advertisement_read")
+    template_name_suffix = "_update"
+    form_class = AdvertisementForm
+
+    def get_success_url(self):
+        return reverse(
+            "advertisement:advertisement_read",
+            kwargs={"pk": self.object.pk},
+        )
+
+    def test_func(self):
+        return self.check_permission("add_advertisement")
 
 
-class AdvertisementDeleteView(DeleteView, AdvertisementView):
-    pass
+class AdvertisementDeleteView(AdvertisementView, DeleteView):
+    def form_valid(self, form):
+        success_url = self.get_success_url()
+        self.object.is_active = False
+        self.object.save()
+        return HttpResponseRedirect(success_url)
+
+    def get_success_url(self):
+        return reverse(
+            "advertisement:advertisement_list",
+        )
+
+    def test_func(self):
+        return self.check_permission("add_advertisement")
 
 
-class AdvertisementListView(ListView, AdvertisementView):
-    queryset = Advertisement.objects.filter(is_active=True)
-    context_object_name = "ads"
+class AdvertisementListView(AdvertisementView, ListView):
+    queryset = Advertisement.objects.filter(is_active=True).only("name")
+    context_object_name = "advertisements"
+
+    def test_func(self):
+        return self.check_permission("add_advertisement")
 
 
-class AdvertisementStaticsView(View):
-    pass
+class AdvertisementStaticsView(AdvertisementView):
+    #  TODO заполнить context
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            "ads/advertisement_statistics.html",
+            # {
+            # ...
+            # },
+        )
+
+    def test_func(self):
+        return self.check_permission("add_advertisement")
